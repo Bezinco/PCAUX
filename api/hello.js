023aed68f2239c28ec2ada2ecd9f3b312c1938e4
Bezinco <141798620+Bezinco@users.noreply.github.com>
2026-03-11 15:57:25 +0800

    Delete api/hello.js

--- api/hello.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: 'Hello World' });
-}
-  // 404 for everything else
-  else {
-    res.status(404).json({ error: 'Not found' });
-  }
-}
